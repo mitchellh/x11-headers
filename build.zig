2ffbd62d82ff73ec929dd8de802bc95effa0ef88
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-04-18 14:29:46 -0700

    use b.path

--- build.zig
+++ build.zig
@@ -12,10 +12,10 @@ pub fn build(b: *std.Build) void {
     });
 
     // contains only GLX headers!
-    lib.installHeadersDirectory(.{ .path = "GL" }, "GL", .{});
-    lib.installHeadersDirectory(.{ .path = "X11" }, "X11", .{});
-    lib.installHeadersDirectory(.{ .path = "xcb" }, "xcb", .{});
-    lib.installHeadersDirectory(.{ .path = "xkbcommon" }, "xkbcommon", .{});
+    lib.installHeadersDirectory(b.path("GL"), "GL", .{});
+    lib.installHeadersDirectory(b.path("X11"), "X11", .{});
+    lib.installHeadersDirectory(b.path("xcb"), "xcb", .{});
+    lib.installHeadersDirectory(b.path("xkbcommon"), "xkbcommon", .{});
 
     b.installArtifact(lib);
 }
